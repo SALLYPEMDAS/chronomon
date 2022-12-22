8417eb6ea9c5e75cc4f0f729c7a9afd94800d1f3
Brian Danowski <sallypemdas@gmail.com>
2022-12-22 00:02:23 -0500

    failing tewst

--- pkg/mon/mon.go
+++ pkg/mon/mon.go
@@ -2,6 +2,32 @@ package mon
 
 import "fmt"
 
+type Planet int64
+
+
+const (
+	Saturn Planet = iota
+	Jupeter
+	Mars
+	Sun
+	Venus
+	Mercury
+	Moon
+)
+
 func Mon () {
 fmt.Println("hello world")
+}
+
+func Color(planet Planet) string {
+
+switch(planet){
+case Saturn:
+return "black";
+
+default:
+return "-1";
+
+}
+
 }
--- pkg/mon/mon_test.go
+++ pkg/mon/mon_test.go
@@ -9,8 +9,16 @@ import (
 
 var _ = Describe("Books", func() {
 It("test", func() {
-        Expect(true).To(Equal(true))
-mon.Mon();
+        Expect(mon.Color(mon.Saturn)).To(Equal("black"))
+        Expect(mon.Color(mon.Jupeter)).To(Equal("blue"))
+        Expect(mon.Color(mon.Mars)).To(Equal("red"))
+        Expect(mon.Color(mon.Mars)).To(Equal("red"))
+        Expect(mon.Color(mon.Sun)).To(Equal("yellow"))
+        Expect(mon.Color(mon.Venus)).To(Equal("green"))
+        Expect(mon.Color(mon.Mercury)).To(Equal("rainbow"))
+        Expect(mon.Color(mon.Moon)).To(Equal("silver"))
+
+
 
 })
 
