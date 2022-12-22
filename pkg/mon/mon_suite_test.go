5228f3d6b2c06c55afb6d5f26ffd8499c1df12a8
Brian Danowski <sallypemdas@gmail.com>
2022-12-21 23:52:15 -0500

    init test

---
+++ pkg/mon/mon_suite_test.go
@@ -0,0 +1,12 @@
+package mon_test
+
+import (
+  . "github.com/onsi/ginkgo/v2"
+  . "github.com/onsi/gomega"
+  "testing"
+)
+
+func TestBooks(t *testing.T) {
+  RegisterFailHandler(Fail)
+  RunSpecs(t, "Books Suite")
+}
---
+++ pkg/mon/mon_test.go
@@ -0,0 +1,17 @@
+package mon_test
+
+import (
+  . "github.com/onsi/ginkgo/v2"
+  . "github.com/onsi/gomega"
+
+  "github.com/SALLYPEMDAS/chronomon/pkg/mon"
+)
+
+var _ = Describe("Books", func() {
+It("test", func() {
+        Expect(true).To(Equal(true))
+mon.Mon();
+
+})
+
+})
